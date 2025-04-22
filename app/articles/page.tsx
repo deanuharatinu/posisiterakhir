2f2119f29bc79a3b87c3eb10fe90667f7ac97295
deanuharatinu <deanu.alt@gmail.com>
2025-04-22 11:34:28 +0700

    feat: hover on article component

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,14 +1,19 @@
-import Link from "next/link";
 import Article from "../ui/article";
 
 export default function Page() {
   return (
-    <div className="flex flex-col" >
-      <h1 className="text-5xl/tight font-bold">Writing on software design, company building, and the aerospace industry.</h1>
-      <p className="text-zinc-400 mt-8">
-        All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order.
-      </p>
-      <div className="mt-16">
+    <div className="flex flex-col lg:max-w-3xl" >
+      <div className="flex flex-col lg:max-w-2xl">
+        <h1 className="text-5xl/tight font-bold tracking-tight">Writing on software design, company building, and the aerospace industry.</h1>
+        <p className="text-zinc-400 mt-8">
+          All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order.
+        </p>
+      </div>
+
+      <div className="flex flex-col gap-10 mt-16">
+        <Article />
+        <Article />
+        <Article />
         <Article />
       </div>
     </div >
--- app/ui/article.tsx
+++ app/ui/article.tsx
@@ -1,37 +1,35 @@
-import Link from "next/link";
-
 export default function Article() {
   return (
-
-    <div className="border-l-2 border-zinc-700/70 pl-6">
-      <article className="grid grid-cols-4">
-        <div className="text-sm text-zinc-500">
-          June 18, 2024
-        </div>
-        <div className="col-span-3 flex flex-col rounded-2xl">
-          <h2 className="font-bold">
-            Make managing dotiles a breeze with Stow
+    <div className="md:border-l-2 md:border-zinc-700/70 md:pl-6">
+      <article className="md:grid md:grid-cols-4 md:items-baseline">
+        <div className="md:col-span-3 group relative flex flex-col">
+          <h2 className="text-base font-semibold tracking-tight text-zinc-100">
+            <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:rounded-2xl bg-zinc-800/50">
+            </div>
+            <a href="/articles/make-managing-dotfiles-a-breeze-with-stow">
+              <span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
+              <span className="relative z-10">Make managing dotfiles a breeze with Stow</span>
+            </a>
           </h2>
-          <p className="my-4 text-sm/relaxed text-zinc-400">
+
+          <time className="md:hidden relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 dark:text-zinc-500 pl-3.5" dateTime="2024-06-18">
+            <span className="absolute inset-y-0 left-0 flex items-center" aria-hidden="true">
+              <span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"></span>
+            </span>June 18, 2024
+          </time>
+
+          <p className="relative z-10 mt-2 text-sm/relaxed text-zinc-600 dark:text-zinc-400">
             For the longest time, I managed my dotfiles with copy and paste. It was really inconvenient to recreate my dotfiles over and over again. That was until I discovered GNU Stow. With Stow, I keep my dotfiles in a remote Git repository, and it has changed the way I manage my dotfiles forever.
           </p>
-          <div className="flex flex-row items-center">
-            <Link
-              className="text-sm text-cyan-500"
-              href="#">
-              Read article
-            </Link>
-            <svg
-              className="ml-1 h-4 w-4 stroke-cyan-500"
-              viewBox="0 0 16 16"
-              fill="none"
-              aria-hidden="true"
-            >
-              <path d="M6.75 5.75 9.25 8l-2.5 2.25" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
-              </path>
+
+          <div aria-hidden="true" className="relative z-10 mt-4 flex items-center text-sm font-medium text-cyan-500">
+            Read article
+            <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" className="ml-1 h-4 w-4 stroke-current">
+              <path d="M6.75 5.75 9.25 8l-2.5 2.25" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
             </svg>
           </div>
         </div>
+        <time className="mt-1 hidden md:block relative z-10 order-first mb-3 items-center text-sm text-zinc-400 dark:text-zinc-500" dateTime="2024-06-18">June 18, 2024</time>
       </article>
     </div>
   )
