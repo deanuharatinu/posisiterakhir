63f42599458f151ea97c4d6c89d4cc8688c113c6
deanuharatinu <deanu.alt@gmail.com>
2025-04-23 11:12:10 +0700

    feat: hide dark mode toggle

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -16,6 +16,7 @@ const links = [
 
 export default function Header({ className = '' }: Readonly<{ className?: string }>) {
   const pathname = usePathname();
+  const showDarkModeToggle = false;
 
   return (
     <InnerContainer>
@@ -35,17 +36,10 @@ export default function Header({ className = '' }: Readonly<{ className?: string
           </div>
 
           <div className="flex justify-end">
-            <div className="bg-zinc-800 rounded-full shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-700/90 hover:ring-zinc-500 transition duration-300">
-              <button className="px-2 py-2 cursor-pointer">
-                <svg xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24" strokeWidth={1.5}
-                  className="size-6 stroke-zinc-400"
-                >
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
-                </svg>
-              </button>
-            </div>
+            {
+              showDarkModeToggle && <DarkModeToggle />
+            }
+
           </div>
         </div>
       </header>
@@ -78,4 +72,20 @@ function NavBar({ pathname = '' }: Readonly<{ pathname?: string }>) {
       </div>
     </div>
   );
+}
+
+function DarkModeToggle() {
+  return (
+    <div className="bg-zinc-800 rounded-full shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-700/90 hover:ring-zinc-500 transition duration-300">
+      <button className="px-2 py-2 cursor-pointer">
+        <svg xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24" strokeWidth={1.5}
+          className="size-6 stroke-zinc-400"
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
+        </svg>
+      </button>
+    </div>
+  )
 }
