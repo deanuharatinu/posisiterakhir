fb9c106b12a95b53e248134ee8860f245982fb7d
deanuharatinu <deanu.alt@gmail.com>
2025-04-23 16:24:58 +0700

    feat: add `ProjectCard` component

---
+++ app/ui/project-card.tsx
@@ -0,0 +1,51 @@
+import { getTechStackIconPath } from "../utils/utils";
+
+export default function ProjectCard({ project = {} }: Readonly<{ project?: { title?: string, date?: string, description?: string, techStack?: string[] } }>) {
+  return (
+    <div className="flex flex-col group relative h-full">
+      <h2 className="text-base font-semibold tracking-tight text-zinc-100">
+        <div className="absolute -inset-x-4 -inset-y-4 z-0 scale-95 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-3 sm:rounded-2xl bg-zinc-800/50">
+        </div>
+        <div>
+          <span className="relative z-10">{project.title}</span>
+        </div>
+      </h2>
+      <h3 className="relative z-10 text-sm text-zinc-400 mt-1">{project.date}</h3>
+      <p className="relative z-10 mt-2 text-sm tracking-wide text-zinc-400">
+        {project.description}
+      </p>
+      <div className="relative z-10 mt-4">
+        <h2 className="text-sm tracking-wide font-bold">
+          Tech Stack
+        </h2>
+        <div className="relative mt-2">
+          <div className="grid grid-cols-[repeat(auto-fill,_minmax(50px,50px))] gap-y-2 gap-x-2">
+            {
+              project.techStack?.map((techStack, index) => {
+                return (
+                  <TechStack key={`${techStack} + ${index}`} techStack={techStack} />
+                );
+              })
+            }
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function TechStack({ techStack }: Readonly<{ techStack?: string }>) {
+  const iconPath = getTechStackIconPath(techStack);
+
+  if (iconPath == "") {
+    return <></>
+  }
+
+  return (
+    <div className="relative z-10 flex p-1 rounded-4xl bg-zinc-800/50 w-[50px] h-[50px] ring-1 ring-zinc-700/60 justify-center">
+      <img
+        className=""
+        src={iconPath} alt={`${techStack} logo`} height={30} width={30} />
+    </div>
+  );
+}
