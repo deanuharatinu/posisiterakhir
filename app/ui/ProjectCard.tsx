4bff698619a2c88fe625ee6d46237f55bc38b9f1
deanuharatinu <deanu.alt@gmail.com>
2025-04-24 11:10:15 +0700

    feat: implement link to project on `ProjectCard`

--- app/ui/ProjectCard.tsx
+++ app/ui/ProjectCard.tsx
@@ -1,8 +1,24 @@
 import { getTechStackIconPath } from "../utils/utils";
 
-export default function ProjectCard({ project = {} }: Readonly<{ project?: { title?: string, date?: string, description?: string, techStack?: string[] } }>) {
+export default function ProjectCard({ project = {} }: Readonly<{ project?: Project }>) {
   return (
-    <div className="flex flex-col group relative h-full">
+    project.link ? (
+      <a
+        href={project.link}
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label={`Visit ${project.title}`}
+      >
+        <CardContent project={project} />
+      </a>
+    ) :
+      (<CardContent project={project} />)
+  );
+}
+
+function CardContent({ project }: Readonly<{ project: Project }>) {
+  return (
+    < div className="flex flex-col group relative h-full" >
       <h2 className="text-base font-semibold tracking-tight text-zinc-100">
         <div className="absolute -inset-x-4 -inset-y-4 z-0 scale-95 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-3 sm:rounded-2xl bg-zinc-800/50">
         </div>
@@ -31,7 +47,7 @@ export default function ProjectCard({ project = {} }: Readonly<{ project?: { tit
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 function TechStack({ techStack }: Readonly<{ techStack?: string }>) {
