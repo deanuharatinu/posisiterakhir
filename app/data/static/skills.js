8227bde701716c2f8b3be2a4def71cf4455503c5
deanuharatinu <deanu.alt@gmail.com>
2025-04-24 15:33:32 +0700

    feat: add static data `menus` & `skills`

---
+++ app/data/static/skills.js
@@ -0,0 +1,36 @@
+export const programmingLanguage = [
+  "Java",
+  "Kotlin",
+  "Swift",
+  "Golang",
+  "Javascript",
+  "Typescript",
+  "Dart",
+  "HTML",
+  "CSS",
+];
+
+export const cloudDevsecops = [
+  "AWS",
+  "Supabase",
+  "Docker",
+  "SonarQube",
+  "Github",
+  "Bitbucket",
+  "Gitlab",
+  "Git"
+];
+
+export const framework = ["Flutter", "ReactJS", "NextJS", "TailwindCSS"];
+
+export const tools = ["Figma", "Jira"];
+
+export const ides = ["Android-Studio", "Xcode", "VScode", "IntelliJ"];
+
+export const technologies = [
+  "NodeJS",
+  "PostgreSQL",
+  "MySQL",
+  "SQLite",
+  "Firebase",
+];
