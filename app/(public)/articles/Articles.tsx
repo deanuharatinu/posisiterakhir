db7a8edda99454782c503a5a5dd7f63eece4f827
deanuharatinu <deanu.alt@gmail.com>
2025-05-01 17:55:41 +0700

    feat: fetch articles from supabase

--- app/(public)/articles/Articles.tsx
+++ app/(public)/articles/Articles.tsx
@@ -1,18 +1,40 @@
-import { formatMillisToDate } from "../../utils/utils";
+import { createClient } from "@/app/utils/supabase/server";
+import { formatTimestampToDate } from "../../utils/utils";
+import { Article as ArticleModel } from "@/app/data/models/article.model";
 
-export default function Articles({ articles }: Readonly<{ articles?: { titles?: string, dateMillis?: string, content?: string, slug?: string }[] }>) {
+export default async function Articles() {
+  const supabase = await createClient()
+
+  const { data, error } = await supabase
+    .from('articles')
+    .select()
+
+  if (error) {
+    console.log(error)
+  }
+
+
+  let articles: ArticleModel[] = (data?.map((article) => {
+    return {
+      title: article.title,
+      dateMillis: article.created_at,
+      content: article.content,
+      slug: article.slug
+    };
+  })) ?? [];
+
   const isNotEmpty = (articles?.length ?? 0) > 0;
 
   return isNotEmpty ? (
     <div className="flex flex-col gap-10">
       {articles?.map((article, index) => (
-        <Article key={`${article.titles}-${index}`} article={article} />
+        <Article key={`${article.title}-${index}`} article={article} />
       ))}
     </div>
   ) : <></>;
 }
 
-function Article({ article }: Readonly<{ article: { title?: string, dateMillis?: string, content?: string, slug?: string } }>) {
+function Article({ article }: Readonly<{ article: ArticleModel }>) {
   return (
     <div className="md:border-l-2 md:border-zinc-700/70 md:pl-6">
       <article className="md:grid md:grid-cols-4 md:items-baseline">
@@ -20,7 +42,8 @@ function Article({ article }: Readonly<{ article: { title?: string, dateMillis?:
           <h2 className="text-base font-semibold tracking-tight text-zinc-100">
             <div className="absolute -inset-x-4 -inset-y-6 z-0 scale-95 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 sm:-inset-x-6 sm:rounded-2xl bg-zinc-800/50">
             </div>
-            <a href="/articles/make-managing-dotfiles-a-breeze-with-stow">
+            {/* <a href="/articles/make-managing-dotfiles-a-breeze-with-stow"> */}
+            <a href={`/articles/${article.slug}`}>
               <span className="absolute -inset-x-4 -inset-y-6 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
               <span className="relative z-10">{article.title}</span>
             </a>
@@ -30,7 +53,7 @@ function Article({ article }: Readonly<{ article: { title?: string, dateMillis?:
             <span className="absolute inset-y-0 left-0 flex items-center" aria-hidden="true">
               <span className="h-4 w-0.5 rounded-full bg-zinc-500"></span>
             </span>
-            {formatMillisToDate(article.dateMillis)}
+            {formatTimestampToDate(article.dateMillis)}
           </time>
 
           <p className="relative z-10 mt-2 text-sm/relaxed text-zinc-400">
@@ -45,7 +68,7 @@ function Article({ article }: Readonly<{ article: { title?: string, dateMillis?:
           </div>
         </div>
         <time className="mt-1 hidden md:block relative z-10 order-first mb-3 items-center text-sm text-zinc-500" dateTime="2024-06-18">
-          {formatMillisToDate(article.dateMillis)}
+          {formatTimestampToDate(article.dateMillis)}
         </time>
       </article>
     </div>
