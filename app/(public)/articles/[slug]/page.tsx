51efe07b21f14b17295b54b78d36f4f09f65508d
deanuharatinu <deanu.alt@gmail.com>
2025-05-01 18:08:03 +0700

    feat: fetch article on article page

--- app/(public)/articles/[slug]/page.tsx
+++ app/(public)/articles/[slug]/page.tsx
@@ -1,5 +1,22 @@
+import { createClient } from "@/app/utils/supabase/server"
+import { notFound } from "next/navigation"
+
+export const dynamic = 'force-dynamic'
+
 export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params
+
+  const supabase = await createClient()
+  const { data, error } = await supabase
+    .from('articles')
+    .select()
+    .eq('published', true)
+    .eq('slug', slug)
+
+  if (error || data.length == 0) {
+    console.log(error)
+    notFound()
+  }
 
   return (
     <div>My Post: {slug}</div>
