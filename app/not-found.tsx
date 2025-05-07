b74d515a4e63d0f0ee6cd9e8471dccf300bc3f2f
deanuharatinu <deanu.alt@gmail.com>
2025-05-07 15:29:57 +0700

    feat: add custom not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,16 @@
+export default function Page() {
+  return (
+    <div className="h-screen flex flex-col justify-center items-center">
+      <div className="flex flex-row gap-3 items-center">
+        <div className="text-3xl font-bold">
+          404
+        </div>
+        <div className="h-10 w-[2px] bg-zinc-600">
+        </div>
+        <div>
+          Page Not Found
+        </div>
+      </div>
+    </div >
+  )
+}
