f9426f07d8a637085e6104b472b7e21b95352dd5
deanuharatinu <deanu.alt@gmail.com>
2025-05-09 13:17:04 +0700

    feat: add search & new article component

--- app/admin/(dashboard)/articles/page.tsx
+++ app/admin/(dashboard)/articles/page.tsx
@@ -22,12 +22,33 @@ export default async function Page(props: {
   return (
     <div className="bg-neutral-800 p-6 rounded-2xl">
       <div className="flex flex-col">
-        <h1 className="font-bold text-2xl">
-          Articles
-        </h1>
+        <div className="flex flex-row justify-between items-center">
+          <h1 className="font-bold text-2xl">
+            Articles
+          </h1>
+          <button className="flex flex-row items-center gap-2 bg-blue-500 font-bold py-3 px-4 rounded-lg cursor-pointer hover:bg-blue-400 transition-colors">
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+              </svg>
+            </span>Add new
+          </button>
+        </div>
 
         <div className="mt-10 flex flex-col">
-          <div className="overflow-x-auto">
+          <form className="relative">
+            <input
+              placeholder="Search"
+              maxLength={32}
+              className="min-w-full bg-neutral-700 rounded-lg py-3 px-3 text-sm text-neutral-300 focus:bg-transparent focus:ring-2 focus:ring-blue-500 outline-none transition"
+            />
+            <button className="absolute right-4 top-1/2 -translate-y-1/2">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+              </svg>
+            </button>
+          </form>
+          <div className="overflow-x-auto mt-4">
             <table className="min-w-full divide-y-1 divide-neutral-500">
               <thead className="text-left">
                 <tr className="font-medium">
