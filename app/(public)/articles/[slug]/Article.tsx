f05714c2379dba3507cac7ea8ac89995643e389d
deanuharatinu <deanu.alt@gmail.com>
2025-05-20 09:33:46 +0700

    feat: implement md-editor

--- app/(public)/articles/[slug]/Article.tsx
+++ app/(public)/articles/[slug]/Article.tsx
@@ -1,15 +1,29 @@
 'use client'
 
-import dynamic from "next/dynamic"
-import data from "./data.json"
+import { Article as ArticleModel } from "@/app/lib/models/article.model"
+import { formatTimestampToDate } from "@/app/utils/utils"
+import MDEditor from "@uiw/react-md-editor"
 
-const Editor = dynamic(() => import("../../../utils/Editor").then((mod) => mod.default), { ssr: false });
-
-export default function Article() {
+export default function Article({ article }: { article: ArticleModel }) {
   return (
     <div className="flex justify-center">
       <div className="flex-auto px-6 lg:max-w-3xl">
-        <Editor data={data} readOnly={true} />
+        <div className="flex flex-row items-center">
+          <div className="bg-neutral-700 w-0.5 rounded-2xl h-5"></div>
+          <p className="text-neutral-500 ml-3">
+            {formatTimestampToDate(article.createdAt)}
+          </p>
+        </div>
+
+        <h1 className="font-bold text-5xl mt-8">
+          {article?.title}
+        </h1>
+        <div className="mt-10">
+          <MDEditor.Markdown
+            source={article.content}
+            style={{ backgroundColor: 'transparent' }}
+          />
+        </div>
       </div>
     </div>
   )
